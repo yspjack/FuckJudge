5af9c32c455d984545887b17fe238b4ac4e3a3df
yspjack <yspjack@gmail.com>
2018-05-28 00:34:53 +0800

    Format code

--- FuckJudge.user.js
+++ FuckJudge.user.js
@@ -8,26 +8,27 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
-    var a=document.querySelector("iframe[name='showmessage']");
-    var b=a.getAttribute("src");
-    var c=b.split("?")[1];
+    var a = document.querySelector("iframe[name='showmessage']");
+    var b = a.getAttribute("src");
+    var c = b.split("?")[1];
     // Append hyper link on the right
-    a.onload=function(){
-        var d=a.contentDocument.querySelector("table a");
-        var detail=document.createElement("a");
-        var e="http://judge.buaa.edu.cn/assignment/judgeDetailsRedirect.jsp?"+c;
-        detail.setAttribute("href",e);
-        detail.innerHTML="<span style=\"margin-left:10%\">More detail</span>";
-        detail.setAttribute("target","_blank")
+    a.onload = function () {
+        var d = a.contentDocument.querySelector("table a");
+        var detail = document.createElement("a");
+        var e = "http://judge.buaa.edu.cn/assignment/judgeDetailsRedirect.jsp?" + c;
+        detail.setAttribute("href", e);
+        detail.innerHTML = "<span style=\"margin-left:10%\">More detail</span>";
+        detail.setAttribute("target", "_blank")
         d.appendChild(detail);
 
-        e="http://judge.buaa.edu.cn/assignment/programPerf/judgeReport.jsp?"+c;
-        detail=document.createElement("a");
-        detail.setAttribute("href",e);
-        detail.innerHTML="<span style=\"margin-left:10%\">Performance</span>";
-        detail.setAttribute("target","_blank")
-        d.appendChild(detail);
+        var perfRank = document.createElement("a");
+        var perfUrl = "http://judge.buaa.edu.cn/assignment/programPerf/judgeReport.jsp?" + c;
+        perfRank = document.createElement("a");
+        perfRank.setAttribute("href", perfUrl);
+        perfRank.innerHTML = "<span style=\"margin-left:10%\">Performance</span>";
+        perfRank.setAttribute("target", "_blank")
+        d.appendChild(perfRank);
     };
 })();
