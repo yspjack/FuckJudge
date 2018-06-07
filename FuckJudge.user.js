14b3727d429215eb47a5d157c6514898fc6947f3
yspjack <yspjack@gmail.com>
2018-06-07 22:56:32 +0800

    Fix bugs

--- FuckJudge.user.js
+++ FuckJudge.user.js
@@ -7,8 +7,8 @@
 // @match        *://judge.buaa.edu.cn/assignment/programList.jsp*
 // @match        *://judge.e.buaa.edu.cn/assignment/programList.jsp*
 // @grant        none
-// @supportURL   https://github.com/wfcrs/FuckJudge/issues
-// @updateURL    https://github.com/wfcrs/FuckJudge/raw/master/FuckJudge.user.js
+// @supportURL   https://github.com/yspjack/FuckJudge/issues
+// @updateURL    https://github.com/yspjack/FuckJudge/raw/ver_href/FuckJudge.user.js
 // ==/UserScript==
 
 (function () {
@@ -20,18 +20,19 @@
     a.onload = function () {
         var d = a.contentDocument.querySelector("table a");
         var detail = document.createElement("a");
-        var e = "http://judge.buaa.edu.cn/assignment/judgeDetailsRedirect.jsp?" + c;
+        var websitePrefix = window.location.href.match(/\/\/j.*\.buaa\.edu\.cn/i)[0];
+        var e = websitePrefix + "/assignment/judgeDetailsRedirect.jsp?" + c;
         detail.setAttribute("href", e);
         detail.innerHTML = "<span style=\"margin-left:10%\">More detail</span>";
-        detail.setAttribute("target", "_blank")
+        detail.setAttribute("target", "_blank");
         d.appendChild(detail);
 
         var perfRank = document.createElement("a");
-        var perfUrl = "http://judge.buaa.edu.cn/assignment/programPerf/judgeReport.jsp?" + c;
+        var perfUrl = websitePrefix + "/assignment/programPerf/judgeReport.jsp?" + c;
         perfRank = document.createElement("a");
         perfRank.setAttribute("href", perfUrl);
         perfRank.innerHTML = "<span style=\"margin-left:10%\">Performance</span>";
-        perfRank.setAttribute("target", "_blank")
+        perfRank.setAttribute("target", "_blank");
         d.appendChild(perfRank);
     };
 })();
