b41f9d7364b8a348a61d386b607a6b38bdb5eead
yspjack <yspjack@gmail.com>
2018-06-20 23:08:46 +0800

    Add background color for results

--- FuckJudge.user.js
+++ FuckJudge.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         FuckJudge
 // @namespace    https://github.com/wfcrs/FuckJudge
-// @version      0.11
+// @version      0.14
 // @description  try to take over the world!
-// @author       btapple
+// @author       btapple & yspjack
 // @match        *://judge.buaa.edu.cn/assignment/programList.jsp*
 // @match        *://judge.e.buaa.edu.cn/assignment/programList.jsp*
 // @match        *://judge.buaa.edu.cn/assignment/index.jsp*
@@ -13,29 +13,63 @@
 // @updateURL    https://github.com/yspjack/FuckJudge/raw/ver_href/FuckJudge.user.js
 // ==/UserScript==
 
-(function () {
-    function init_checker() {
-        var option_list = document.getElementsByClassName("option");
-        var i = 0;
-        for (i = 0; i < option_list.length; i++) {
-            if (option_list[i].value.trim() === "") {
-                option_list[i].setAttribute("style", "background-color:red;")
-            } else {
-                option_list[i].removeAttribute("style");
-            }
-        }
-        var cloze_list = document.getElementsByClassName("cloze");
-        for (i = 0; i < cloze_list.length; i++) {
-            if (cloze_list[i].value.trim() === "") {
-                cloze_list[i].removeAttribute("style");
-            } else {
-                cloze_list[i].removeAttribute("style");
-            }
+function empty_checker() {
+    console.log("empty_checker works");
+    var option_list = document.getElementsByClassName("option");
+    var i = 0;
+    for (i = 0; i < option_list.length; i++) {
+        if (option_list[i].value.trim() === "") {
+            option_list[i].setAttribute("style", "background-color:red;")
+        } else {
+            option_list[i].removeAttribute("style");
         }
     }
+    var cloze_list = document.getElementsByClassName("cloze");
+    for (i = 0; i < cloze_list.length; i++) {
+        if (cloze_list[i].value.trim() === "") {
+            cloze_list[i].setAttribute("style", "background-color:red;")
+        } else {
+            cloze_list[i].removeAttribute("style");
+        }
+    }
+}
+
+function AC_checker_index() {
+    console.log("AC_checker works");
+    var result_list = document.querySelectorAll(".tableline .tableline2 .formtext table td");
+    console.log(result_list.length);
+    for (i = 0; i < result_list.length; i++) {
+        if (result_list[i].innerText.indexOf("运行时错误") != -1) {
+            result_list[i].setAttribute("style", "background-color:#8e44ad;color:#ffffff;");
+        } else if (result_list[i].innerText === "完全正确") {
+            result_list[i].setAttribute("style", "background-color:#5eb95e;color:#ffffff;");
+        } else if (result_list[i].innerText === "答案错误") {
+            result_list[i].setAttribute("style", "background-color:#e74c3c;color:#ffffff;");
+        }
+    }
+}
+
+function AC_checker_program() {
+    console.log("AC_checker works");
+    var parent = document.querySelector("iframe[name='showmessage']");
+    var result_list = parent.contentDocument.querySelectorAll("table table td");
+    console.log(result_list.length);
+    for (i = 0; i < result_list.length; i++) {
+        if (result_list[i].innerText.indexOf("运行时错误") != -1) {
+            result_list[i].setAttribute("style", "background-color:#8e44ad;color:#ffffff;");
+        } else if (result_list[i].innerText === "完全正确") {
+            result_list[i].setAttribute("style", "background-color:#5eb95e;color:#ffffff;");
+        } else if (result_list[i].innerText === "答案错误") {
+            result_list[i].setAttribute("style", "background-color:#e74c3c;color:#ffffff;");
+        }
+    }
+}
+
+(function () {
     'use strict';
     if (window.location.href.match("index\\.jsp")) {
-        init_checker();
+        empty_checker();
+        AC_checker_index();
         return;
     }
     var a = document.querySelector("iframe[name='showmessage']");
@@ -43,6 +77,7 @@
     var c = b.split("?")[1];
     // Append hyper link on the right
     a.onload = function () {
+        AC_checker_program();
         var d = a.contentDocument.querySelector("table a");
         var detail = document.createElement("a");
         let websitePrefix = window.location.href.match(/\/\/j.*\.buaa\.edu\.cn/i)[0];
